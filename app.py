bbf12d768b64d3b9d0216205b0f1b2b6846e65d6
Jatin Chhhangani <jatinchhangani@gmail.com>
2022-04-17 23:59:18 +0530

    api

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from cProfile import run
 from distutils.log import debug
 from flask import Flask, render_template, request
+import test as lstm 
 
 app=Flask(__name__)
 
@@ -9,6 +10,15 @@ def index():
     if request.method == 'POST':
         note = request.form["keynote"]
     return render_template('index.html')
+
+@app.route('/result', methods=['POST'])
+def results():
+    body = request.form["start_string"];
+    data = lstm.generate_text(body, generation_length = 2000)
+    return data
+
+    
+
 
 
 if __name__ == "__main__":
