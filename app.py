70a92f90ab372b1fa4cedde1f02d391a7a3412a4
hemanshu68 <hemanshuchaudhari68@gmail.com>
2022-04-17 23:25:44 +0530

    initial setup

---
+++ app.py
@@ -0,0 +1,15 @@
+from cProfile import run
+from distutils.log import debug
+from flask import Flask, render_template, request
+
+app=Flask(__name__)
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        note = request.form["keynote"]
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ test.py
@@ -0,0 +1,46 @@
+import tensorflow as tf
+
+from tqdm import tqdm
+import numpy
+
+
+def generate_text(model, start_string, generation_length=1000):
+  input_eval = [char2idx[s] for s in start_string] 
+  input_eval = tf.expand_dims(input_eval, 0)
+
+ 
+  text_generated = []
+
+  
+  model.reset_states()
+
+  for i in tqdm(range(generation_length)):
+      
+      predictions = model(input_eval)
+      
+      predictions = tf.squeeze(predictions, 0)
+      
+    
+      predicted_id = tf.random.categorical(predictions, num_samples=1)[-1,0].numpy()
+      input_eval = tf.expand_dims([predicted_id], 0)
+      text_generated.append(idx2char[predicted_id])  
+      
+  return (start_string + ''.join(text_generated))
+
+
+model = tf.keras.models.load_model("m.hdf5",compile=False)
+
+
+char2idx = {'\n': 0, ' ': 1, '!': 2, '"': 3, '#': 4, "'": 5, '(': 6, ')': 7, ',': 8, '-': 9, '.': 10, '/': 11, '0': 12, '1': 13, '2': 14, '3': 15, '4': 16, '5': 17, '6': 18, '7': 19, '8': 20, '9': 21, ':': 22, '<': 23, '=': 24, '>': 25, 'A': 26, 'B': 27, 'C': 28, 'D': 29, 'E': 30, 'F': 31, 'G': 32, 'H': 33, 'I': 34, 'J': 35, 'K': 36, 'L': 37, 'M': 38, 'N': 39, 'O': 40, 'P': 41, 'Q': 42, 'R': 43, 'S': 44, 'T': 45, 'U': 46, 'V': 47, 'W': 48, 'X': 49, 'Y': 50, 'Z': 51, '[': 52, ']': 53, '^': 54, '_': 55, 'a': 56, 'b': 57, 'c': 58, 'd': 59, 'e': 60, 'f': 61, 'g': 62, 'h': 63, 'i': 64, 'j': 65, 'k': 66, 'l': 67, 'm': 68, 'n': 69, 'o': 70, 'p': 71, 'q': 72, 'r': 73, 's': 74, 't': 75, 'u': 76, 'v': 77, 'w': 78, 'x': 79, 'y': 80, 'z': 81, '|': 82}
+{'\n': 0, ' ': 1, '!': 2, '"': 3, '#': 4, "'": 5, '(': 6, ')': 7, ',': 8, '-': 9, '.': 10, '/': 11, '0': 12, '1': 13, '2': 14, '3': 15, '4': 16, '5': 17, '6': 18, '7': 19, '8': 20, '9': 21, ':': 22, '<': 23, '=': 24, '>': 25, 'A': 26, 'B': 27, 'C': 28, 'D': 29, 'E': 30, 'F': 31, 'G': 32, 'H': 33, 'I': 34, 'J': 35, 'K': 36, 'L': 37, 'M': 38, 'N': 39, 'O': 40, 'P': 41, 'Q': 42, 'R': 43, 'S': 44, 'T': 45, 'U': 46, 'V': 47, 'W': 48, 'X': 49, 'Y': 50, 'Z': 51, '[': 52, ']': 53, '^': 54, '_': 55, 'a': 56, 'b': 57, 'c': 58, 'd': 59, 'e': 60, 'f': 61, 'g': 62, 'h': 63, 'i': 64, 'j': 65, 'k': 66, 'l': 67, 'm': 68, 'n': 69, 'o': 70, 'p': 71, 'q': 72, 'r': 73, 's': 74, 't': 75, 'u': 76, 'v': 77, 'w': 78, 'x': 79, 'y': 80, 'z': 81, '|': 82}
+
+idx2char = ['\n',' ','!','"','#', "'" ,'(',')',',','-','.','/','0','1','2','3','4','5',
+ '6','7','8','9',':','<','=','>','A','B','C','D','E','F','G','H','I','J',
+ 'K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','[',']',
+ '^','_','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p',
+ 'q','r','s','t','u','v','w','x','y','z','|']
+
+text = generate_text(model, start_string = "XX");
+
+with open('output.txt','w') as f:
+    f.write(text)
