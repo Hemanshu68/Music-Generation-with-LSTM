67281fea36eb81bd4bd798650f35520e3512f6fb
Jatin Chhhangani <jatinchhangani@gmail.com>
2022-04-18 11:26:06 +0530

    fixes:

--- app.py
+++ app.py
@@ -1,24 +1,24 @@
 from cProfile import run
 from distutils.log import debug
 from flask import Flask, render_template, request
+from mitdeeplearning import lab1 as mdl;
+
+import music21 as ms
 import test as lstm 
 
 app=Flask(__name__)
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/', methods=['GET'])
 def index():
-    if request.method == 'POST':
-        note = request.form["keynote"]
     return render_template('index.html')
 
 @app.route('/result', methods=['POST'])
 def results():
     body = request.form["start_string"]
     data = lstm.generate_text(body, generation_length = 2000)
-    return data
-
+    text  = mdl.extract_song_snippet(data)
     
-
+    return render_template('results.html', data = text);
 
 
 if __name__ == "__main__":
